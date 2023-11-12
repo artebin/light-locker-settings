d994bbd0ce8d00f5b9c4773716604134d51924dd
Nicolas James <nicolas.james@gmail.com>
2023-11-12 18:10:21 +0100

    force use gtk 3.0

--- light-locker-settings/light-locker-settings.py
+++ light-locker-settings/light-locker-settings.py
@@ -16,6 +16,9 @@
 #   You should have received a copy of the GNU General Public License along
 #   with this program.  If not, see <http://www.gnu.org/licenses/>.
 
+import gi
+gi.require_version('Gtk', '3.0')
+
 import gettext
 
 gettext.textdomain('light-locker-settings')
